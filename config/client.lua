50917b21b381b585295146d26052b9fb8f974988
Crutchie <github@crutchie.com>
2024-04-11 15:34:37 -0500

    Update client.lua

--- config/client.lua
+++ config/client.lua
@@ -1,3 +1,6 @@
+---@param useTarget boolean set whether to use target
+---@param debugPoly boolean set whether to display a red debug box around the stash location
+
 return {
     useTarget = false,
     debugPoly = false,
